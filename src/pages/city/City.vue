86a1466922eeef7e7c881533e4f454a8d58f70b0
jawsro <roro1990@163.com>
2019-09-28 17:06:52 +0800

    Create Search.vue

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-search></city-search>
     </div>
 </template>
 <script>
 import CityHeader from './components/Header'//引入header.vue组件
+import CitySearch from './components/Search'//引入header.vue组件
 export default{
     name:"City",
     data (){
@@ -13,7 +15,8 @@ export default{
         }
     },
     components:{
-        CityHeader
+        CityHeader,
+        CitySearch
     }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="saerch">
+        <input type="text" placeholder="输入城市名或者拼音" class="saerch-input">
+    </div>
+</template>
+<script>
+
+export default{
+    name:"CitySearch",
+    data (){
+        return {
+           
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    .saerch
+        height: 0.72rem
+        background:#00bcd4
+        padding:0 .1rem
+        .saerch-input
+            box-sizing:border-box
+            width:100%
+            height: 0.62rem
+            list-height: 0.62rem
+            text-align:center
+            border-radius: 0.06rem
+            color: #666
+</style>
